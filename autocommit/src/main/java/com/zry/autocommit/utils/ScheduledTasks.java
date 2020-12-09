ce7e4e65e4bd4364c37a4c7a5bfb8d53f56a76e4
zhangry <zhangry@fapiao.com>
2020-12-09 14:11:45 +0800

    autocommit 2020/12/09 14:11

--- autocommit/src/main/java/com/zry/autocommit/utils/ScheduledTasks.java
+++ autocommit/src/main/java/com/zry/autocommit/utils/ScheduledTasks.java
@@ -8,9 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
